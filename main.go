a32e1d9e98d16bb6acb74cf6f8a2ee591eabfa5e
Joe Arias <jarias@wawand.co>
2024-02-14 14:03:52 -0500

    using env variable to set the port

--- main.go
+++ main.go
@@ -21,9 +21,15 @@ func main() {
 	// This is your test secret API key.
 	stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
 
+	hostAddress := os.Getenv("HOST_ADDRESS")
+	if hostAddress == "" {
+		// Provide a default host address if none is specified
+		hostAddress = "127.0.0.1"
+	}
+
 	r := router.Router()
 
-	fmt.Println("Starting server on the port 3000...")
+	fmt.Printf("Starting server on the host %s port 3000...\n", hostAddress)
 
-	log.Fatal(http.ListenAndServe(":3000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:3000", hostAddress), r))
 }
