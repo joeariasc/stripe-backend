77c79e3e2d24e680e1ea1798d52353d828363a12
Joe Arias <jarias@wawand.co>
2024-02-15 14:06:00 -0500

    adjusting serve config

--- main.go
+++ main.go
@@ -21,15 +21,9 @@ func main() {
 	// This is your test secret API key.
 	stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
 
-	hostAddress := os.Getenv("HOST_ADDRESS")
-	if hostAddress == "" {
-		// Provide a default host address if none is specified
-		hostAddress = "127.0.0.1"
-	}
-
 	r := router.Router()
 
-	fmt.Printf("Starting server on the host %s port 3000...\n", hostAddress)
+	fmt.Println("Starting server on the port 3000...")
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:3000", hostAddress), r))
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
